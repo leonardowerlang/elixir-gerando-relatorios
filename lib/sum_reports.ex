7a36a23f5bfa50ba2e67746291adfe26f10e757f
Leonardo <leonardo@belasis.com.br>
2022-11-28 22:10:04 -0300

    :hammer: Melhora na soma dos relatórios

--- lib/sum_reports.ex
+++ lib/sum_reports.ex
@@ -1,11 +1,17 @@
 defmodule GenReport.SumReports do
   def sum(%{} = report1, %{} = report2) do
-    report1
-    |> Enum.map(fn {key, _value} -> Map.put(%{}, key, sum(report1[key], report2[key])) end)
+    extrac_reports_keys(report1, report2)
+    |> Enum.map(fn key -> Map.put(%{}, key, sum(report1[key], report2[key])) end)
     |> Enum.reduce(%{}, fn acc, report ->  Map.merge(acc, report) end)
   end
 
-  def sum(%{} = report, nil), do: report
+  def sum(report, nil), do: report
+  def sum(nil, report), do: report
 
   def sum(number1, number2), do: number1 + number2
+
+  defp extrac_reports_keys(report1, report2) do
+    Map.keys(report1) ++ Map.keys(report2)
+    |> Enum.uniq()
+  end
 end
